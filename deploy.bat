8af2d6baa345a1c436a02e352456a48c6543f24d
kipia <kipia@server.local>
2026-08-31 12:38:59 +0000

    Удалён мусор: rar/zip архивы, скриншот, deploy.bat, служебные md

--- deploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo =========================================
-echo Deploying to production...
-echo =========================================
-git push production main
-echo =========================================
-echo Done!
-echo =========================================
-pause
